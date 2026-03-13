e59e5fe6ba2a477980d331697e0d53fc06ffa9b1
Ryan Szczesny <ryanszczesny@yahoo.com>
2026-03-13 15:41:47 -0400

    Added commands txt file and first models

--- novaspin/casino/models.py
+++ novaspin/casino/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Blackjack(models.Model):
+    player_score = models.IntegerField()
+    dealer_score = models.IntegerField()
+    player_bet = models.IntegerField()
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    balance = models.IntegerField()
+
+class Slots(models.Model):
+    player_bet = models.IntegerField()
+
+class Roulette(models.Model):
+    player_bet = models.IntegerField()
+    player_number = models.IntegerField()
+    player_color = models.CharField(max_length=5)
